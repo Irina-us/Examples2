083520725073338c30d872996f2a0db17bd5b2ad
Irina <irina@Mac-mini-Irina.local>
2023-06-12 00:20:21 +0500

    S002

--- S002/Program.cs
+++ S002/Program.cs
@@ -7,14 +7,21 @@
 
 Console.Clear();
 
-int num1 = new Random().Next(1,100);
-int num2 = new Random().Next(1,10);
-System.Console.WriteLine($"Были сгенерированы 2 случайных числа {num1} и {num2}");
+// int num1 = new Random().Next(1,100);
+// int num2 = new Random().Next(1,10);
+// System.Console.WriteLine($"Были сгенерированы 2 случайных числа {num1} и {num2}");
+
+System.Console.WriteLine("Введите число 1");
+int num1 = Convert.ToInt32( Console.ReadLine() );
+System.Console.WriteLine("Введите число 2");
+int num2 = Convert.ToInt32( Console.ReadLine() );
 
 int x1 = num1 % num2;
-// System.Console.WriteLine(x1);
 
-if (x1 == 0)
-    System.Console.WriteLine("Кратно");
+if (num1 < num2)
+    System.Console.WriteLine($"{num1} не делится на {num2}");
+else 
+    if (x1 == 0)
+    System.Console.WriteLine($"{num1} кратно {num2}");
 else
-    System.Console.WriteLine($"Не кратно, остаток = {x1}");
+    System.Console.WriteLine($"{num1} не кратно {num2}, остаток = {x1}");

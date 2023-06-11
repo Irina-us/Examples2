926425110a562f4219db7d0f85e941fea239aeca
Irina <irina@Mac-mini-Irina.local>
2023-06-11 23:46:53 +0500

    S003

---
+++ S003/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит случайное
+// трехзначное число и удаляет вторую цифру этого
+// числа.
+// ● 456 -> 46
+// ● 782 -> 72
+// ● 918 -> 98
+
+Console.Clear();
+
+int num = new Random().Next(100,1000);
+System.Console.WriteLine($"Было сгенерировано случайное число {num}");
+
+int x1 = num / 100;
+int x2 = num%10; 
+System.Console.Write($"{x1}{x2}");
+// for (int i=1; i<=100; i++)
+// System.Console.Write($"{i} ");

4e2daf7021aca6f5825041294c5676401eb91b43
Irina <irina@Mac-mini-Irina.local>
2023-06-11 22:55:58 +0500

    S001

--- S001/Program.cs
+++ S001/Program.cs
@@ -1,5 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит случайное число
+// из отрезка [10, 99] и показывает наибольшую цифру числа
+// Например:
+// 78 -> 8
+// 12 -> 2
+// 85 -> 8
 
+Console.Clear();
 
-fhdkgjshdfgnlsdkfg
+int num = new Random().Next(10,100);
+System.Console.WriteLine($"Было сгенерировано случайное число {num}");
+//System.Console.WriteLine("Было сгенерировано случайное число "+(num));
+int x1 = num / 10; // левая цифра двузначного числа = целочисленное деление на 10
+int x2 = num%10; // правая цифра двузначного числа = остаток от деления на 10
+if (x1>x2)
+    System.Console.WriteLine($"Наибольшая цифра этого числа {x1}");
+else
+    if (x1<x2)
+    System.Console.WriteLine($"Наибольшая цифра этого числа {x2}");
+else
+    System.Console.WriteLine($"Цифры равны {x2}");

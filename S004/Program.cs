7f9b180a77832418d0e49d639a265c7999a6d523
Irina <irina@Mac-mini-Irina.local>
2023-06-12 00:39:23 +0500

    S004

---
+++ S004/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход
+// число и проверяет, кратно ли оно одновременно
+// 7 и 23.
+// ● 14 -> нет
+// ● 46 -> нет
+// ● 161 -> да
+
+Console.Clear();
+
+System.Console.WriteLine("Проверим делится ли введеное число на 7 и 23 одновременно");
+System.Console.WriteLine("Введите число");
+int num = Convert.ToInt32( Console.ReadLine() );
+int a = 7;
+int b = 23;
+
+if (num % a ==0 && num % b ==0)
+    System.Console.WriteLine($"делится");
+else 
+    System.Console.WriteLine($"не делится");
